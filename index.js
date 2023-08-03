8bed12422bc3451a0b28a2c50190ff5c41cc119a
chuma-beep <wisboynelson123@gmail.com>
2023-08-03 05:40:45 +0100

    corrected an error

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 //select all checkboxes
-const checkboxes = document.querySelectorAll(".inbox input ")
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+    // check if they had the shift key down
+   //ANd if they are checking it
+
+   let inBetween = false;
+if(e.shiftKey && this.checked) {
+//loop over every single checbox
+checkboxes.forEach(checkbox => {
+    console.log(checkbox);
+    if(checkbox === this || checkbox === lastChecked){
+        inBetween = !inBetween;
+        console.log("Starting to check inbetween")
+    }
+
+   if(inBetween){
+    checkbox.checked = true;
+   }
+
+  });
+
+}
+    lastChecked = this;
+}
+
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
